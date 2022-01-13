2186831eb6ac08542e7ad6cb10a0b469156d8f95
numberx <shekooha696@gmail.com>
2022-01-13 11:29:58 +0330

    Server side ssh implemented

--- sshServer.py
+++ sshServer.py
@@ -4,6 +4,9 @@ import paramiko
 import threading
 import sys
 import getpass
+#TODO: dynamicly get creds 
+#TODO: implement client side of ssh
+
 # e.g. /home/<username>/.ssh/id_rsa.pub
 rsa_key = ""
 #get credentials 
@@ -18,11 +21,12 @@ credentials = {
 key = paramiko.RSAKey(filename=rsa_key)
 
 class Server (paramiko.ServerInterface):
-	def __init__(self):
+	def __init__(self,username, password, kind , chanid, ip):
 		self.event = threading.Event()
+		self.run(self.username, self.password, self.kind , self.chanid, self.ip)
 	
 	def check_request(self, kind, chanid):
-		if kind == 'session':
+		if self.kind == 'session':
 			return paramiko.OPEN_SUCCEEDED
 		return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 	
@@ -34,12 +38,56 @@ class Server (paramiko.ServerInterface):
 		return paramiko.AUTH_FAILED
 
 	def listener(self, ip):
-	""" starting a listener """
+		""" starting a listener """
+		try:
+			session = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			session.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+			session.bind((self.ip, 22))
+			session.listen(100)
+			print ("[*] Listening for connection .....")
+			client , addr = sock.accept()
+			print ("[+] Connection Established")
+		except Exception, e:
+			print ("[-] Connection failed: " + str(e))
+
+	def trasport(self):
+		client = self.listener.client
+		t = paramiko.Trasport(client)		
+		try:
+			t.load_server_moduli()
+		except:
+			print("[-] failed to load moduli")
+			raise
+		t.add_server_key(key)
+		server = Server()
+		try:
+			t.start_server(server=server)
+		except paramiko.SSHExeption, x:
+			print ("[-] SSH hadnshake failed")
+
+		chan = t.accept(20)
+		print ("[+] Authenticated")
+		print (chan.recv(1024))
+		chan.send('CMD TO SEND')
+
+	except Exception, e:
+		print ("[-] Error occured: " str(e.class) + ":" str(e))
 	try:
-		session = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		session.bind((ip, 22))
-		session.listen(100)
-		print ("[*] Listening for connection .....")
-	except Exception as e:
-		print ("")
-		# interuppted by jahd :(
+		t.close()
+	except:
+		pass
+		sys.exit(1)
+
+	def run(self, username, password, kind , chanid, ip):
+		self.check_request(self.kind, chanid)
+		self.check_credentials(self.username,self.password)
+		self.listener(self.ip)
+		self.trasport()
+
+
+
+
+
+
+
+

